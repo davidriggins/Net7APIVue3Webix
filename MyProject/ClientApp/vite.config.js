b7ec660d20ab7c841ebf3d0840c511f1ad208515
David Riggins <driggins@esri.com>
2023-03-02 10:10:47 -0800

    Add logic and cert for https API. Tie Vue into Asp.Net Core.

--- MyProject/ClientApp/vite.config.js
+++ MyProject/ClientApp/vite.config.js
@@ -3,9 +3,24 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import fs from 'node:fs'
+// import { ServerOptions } from 'https'
+
+const developmentCertificateName = 'localhost.pfx'
+
+var httpsSettings = fs.existsSync(developmentCertificateName)
+  ? {
+      pfx: fs.readFileSync(developmentCertificateName),
+      passphrase: 'secret'
+    }
+  : {}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    https: httpsSettings
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,17 +13,34 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller}/{action=Index}/{id?}");
+});
+//app.MapFallbackToFile("index.html");
 
-app.MapFallbackToFile("index.html");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSpa(spa =>
+    {
+        //spa.UseProxyToSpaDevelopmentServer("http://localhost:5173");
+        spa.UseProxyToSpaDevelopmentServer("https://localhost:5173");
+    });
+}
+else
+{
+    app.MapFallbackToFile("index.html");
+}
+
 
 app.Run();
